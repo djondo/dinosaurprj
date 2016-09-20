44c334dca55c77068aa1097e8674f7e846227ead
rudi <kokourudolf@go.minneapolis.edu>
2016-09-20 11:29:04 -0500

    Updated version -- dinosaur file changed

---
+++ src/com/rudolf/Dinosaur.java
@@ -0,0 +1,10 @@
+package com.rudolf;
+
+/**
+ * Created by rudi on 9/20/2016.
+ */
+public class Dinosaur {
+    public void Droar(){
+        System.out.println("GRRR!!!!!");   }
+}
+
---
+++ src/com/rudolf/dinosaurManager.java
@@ -0,0 +1,14 @@
+package com.rudolf;
+
+public class dinosaurManager {
+
+    public static void main(String[] args) {
+        // write your code here
+        Dinosaur t_rex = new Dinosaur();
+        Dinosaur velociraptor = new Dinosaur();
+
+        t_rex.Droar();
+        velociraptor.Droar();
+
+    }
+}
